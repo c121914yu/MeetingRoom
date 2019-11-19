f6b90a99b21db58e44def3d23a098b0c2721014e
jinlongyu <545436317@qq.com>
2019-11-19 09:13:47 +0800

    1

--- MeetingRoom/models.py
+++ MeetingRoom/models.py
@@ -4,3 +4,10 @@ class user(models.Model):
     name = models.CharField(max_length=20)
     password = models.CharField(max_length=50)
     email = models.CharField(max_length=60)
+    ID = models.CharField(max_length=100,primary_key=True,unique=True,default='')
+
+class manager(models.Model):
+    name = models.CharField(max_length=20)
+    email = models.CharField(max_length=60)
+    ID = models.CharField(max_length=100,primary_key=True,unique=True,default='')
+    encode = models.TextField(default='')
